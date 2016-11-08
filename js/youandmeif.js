7aec8a23ea8a3518519d69074a3884d2d0d2abf2
Marc Morales <marcmorales345@gmail.com>
2016-11-08 13:41:53 -0700

    general function for main navigation complete

--- js/youandmeif.js
+++ js/youandmeif.js
@@ -1,36 +1,35 @@
 $(document).ready(function() {
-    var navUl = $("nav .menu");
-    
+    var navUl = $("nav .menu"),
+        myWindow = $(window),
+        windowWidth = myWindow.innerWidth;
+        
     //main navigation menu toggle for mobile and tablet
     $(".navMenu").click(function() {
         navUl.toggleClass("navScroll");
-        
+
         //navbar collapse when mouse leaves navbar
-//        $(this).parent().mouseleave(function() {
-//            navUl.removeClass("navScroll");
-//        });
+//            $(this).parent().mouseleave(function() {
+//                navUl.removeClass("navScroll");
+//            });
     });
-    
-    
-    function navResponsive() {   
+    function navResponsive() {
         var menuItem = $(".menu-item"),
-            subMenu = menuItem.children(".sub-menu"); //find $('li ul')
-    
+            subMenu = menuItem.children(".sub-menu");
+
         //adds icon beside <li> that has sub-links inside
         subMenu.siblings("a").after("<div class='subMenuIcon'><span>+</span></div>");
         var subMenuIcon = $(".subMenuIcon span");
-        
-        //expand and contract sub-item list
+
+        //insert icons for sub-menu
         subMenuIcon.click(function() {
             $(this).parent().siblings(".sub-menu").toggleClass("navScroll");
-            
+
             if ($(this).text() === "+") {
                 $(this).text("-");
             }else{
                 $(this).text("+");
             }
         });
-        
     }
     navResponsive();
 });
